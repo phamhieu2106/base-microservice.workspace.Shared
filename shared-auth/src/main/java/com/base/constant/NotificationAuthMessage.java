ff02554747bf7d7f9edc1db2b4f2934c7e4ba787
Henry <henry.pham@alpaca.vn>
2025-07-01 10:10:05 +0700

    build: update base auth service

--- shared-auth/src/main/java/com/base/constant/NotificationAuthMessage.java
+++
@@ -1,6 +0,0 @@
-package com.base.constant;
-
-public class NotificationAuthMessage {
-
-    public static final String NOTIFICATION_BLOCKED_USER = "NOTIFICATION_BLOCKED_USER";
-}
--- shared-auth/src/main/java/com/base/service/UserDetailsServiceConfig.java
+++ shared-auth/src/main/java/com/base/service/UserDetailsServiceConfig.java
@@ -1,47 +1,19 @@
 package com.base.service;
 
-import com.base.api.InternalAuthQueryApi;
-import com.base.base.exception.ServiceException;
-import com.base.constant.AuthErrorCode;
-import com.base.constant.UserStatus;
-import com.base.model.CustomUserDetails;
-import com.base.response.UserDetailResponse;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.ObjectUtils;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
-import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceConfig implements UserDetailsService {
 
-    private final InternalAuthQueryApi internalAuthQueryApi;
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserDetailResponse userDetail = internalAuthQueryApi.getUserDetailsByUsername(username).getData();
-        if (ObjectUtils.isEmpty(userDetail)) {
-            throw new ServiceException(AuthErrorCode.USER_NOT_FOUND);
-        }
-        if (Objects.equals(userDetail.getStatus(), UserStatus.BLOCKED)) {
-            throw new ServiceException(AuthErrorCode.USER_BLOCKED);
-        }
 
-        CustomUserDetails userDetails = new CustomUserDetails();
-        userDetails.setUsername(username);
-        userDetails.setAuthorities(userDetail.getAuthorities().stream()
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList()));
-        userDetails.setPassword(userDetail.getPassword());
-        userDetails.setStatus(userDetail.getStatus());
-
-        return userDetails;
+        return null;
     }
 
 }
